77eb917dfd8b366584c1b6b1260ac405db38a34e
priteshghaywat <87220640+priteshghaywat@users.noreply.github.com>
2024-11-18 12:24:01 +0530

    Delete terraform directory

--- terraform/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "example" {
-  name     = "terraform-practivece-azf
-  location = "East US"
-}
--- terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "resource_group_name" {
-  default = "my-terraform-rg"
-}
-
-variable "location" {
-  default = "East US"
-}
